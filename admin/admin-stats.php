813cd568dc747a301c75cbf05336ef97e17170c8
Jaziamiza <fairuzemiralifiansyah@gmail.com>
2026-09-23 15:43:54 +0700

    lanjutan

--- admin/admin-stats.php
+++ admin/admin-stats.php
@@ -9,7 +9,6 @@
     <link href="https://fonts.googleapis.com" rel="preconnect"/>
     <link crossorigin="" href="https://fonts.gstatic.com" rel="preconnect"/>
     <link href="https://fonts.googleapis.com/css2?family=Geist:wght@400;500;600;700;800;900&family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet"/>
-    <script src="https://cdn.tailwindcss.com?plugins=forms,container-queries"></script>
     <script src="../shared-config.js"></script>
     <script src="../project-store.js"></script>
     <script src="../intern-store.js"></script>
